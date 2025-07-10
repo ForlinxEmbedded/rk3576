01ce9ad19c34d7f4ef54dd51c5630d208aa69aa8
Jesse Gao <jesse@forinx.com>
2025-07-10 13:36:17 +0800

    Initial commit

---
+++ source/conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'FET3506-C/OK3506-C'
+copyright = '2025, Forlinx Embedded Technology Co., Ltd'
+author = 'Forlinx Embedded Technology Co., Ltd'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_copy_source = False
+
---
+++ source/index.rst
@@ -0,0 +1,18 @@
+.. RK3576 documentation master file, created by
+   sphinx-quickstart on Sat Jul  5 15:18:46 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+FET3506-C / OK3506-C Linux 5.1.0 User Manual
+====================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   Hardware
+   :maxdepth: 3
+   :caption: Contents
+
